18949b5dfe2db421d0e8550af5993ad2f502f741
Victor <vctrd3@gmail.com>
2020-07-16 19:26:35 -0400

    added schema

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use('/graphql', graphqlHTTP({
+const conn = async () => {
+  try{
+    await mongoose.connect(keys.dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
+    console.log('connected to mongodb')
+    app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
+  }catch{}
+}
+  
+conn()
 
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
 }))
 
-app.listen(3000, () => console.log('server is listening on port 3000'))
