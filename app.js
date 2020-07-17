9212b82f6325b2c7bd3154bfbee7a8af027a1080
Victor <vctrd3@gmail.com>
2020-07-16 23:05:17 -0400

    connect react with graphql server

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
 
+const { ApolloServer } = require('apollo-server-express')//setup apollo
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+const server = new ApolloServer({ schema })//setup apollo
+server.applyMiddleware({ app })//setup apollo
 
 const conn = async () => {
   try{
